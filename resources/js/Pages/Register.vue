639beef0d74165cbb7282d64d7025ed7e9903ef8
MacroTom <okeesieba@gmail.com>
2023-04-25 23:26:52 +0100

    Create register page

---
+++ resources/js/Pages/Register.vue
@@ -0,0 +1,15 @@
+<script>
+import { Head } from '@inertiajs/vue3';
+export default {
+    data(){
+        return{}
+    }
+}
+</script>
+
+<template>
+    <Head>
+        <title>Register</title>
+    </Head>
+    <p class="text-xl font-medium">Register via email and phone</p>
+</template>
